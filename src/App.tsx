3d6bf3a04283ebf7c7ac84a78e777d74e75209e1
guillesrl <gwnqn@hotmail.com>
2025-10-04 20:11:20 +0200

    Ordenes Activas corregido

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -15,13 +26,12 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <RouterProvider 
+          router={router} 
+          future={{
+            v7_startTransition: true,
+          }} 
+        />
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
